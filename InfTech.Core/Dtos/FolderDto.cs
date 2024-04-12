759ce8dd8031b459b5ac86607a3ffa662c1b3815
ferry <74259838+VladUl287@users.noreply.github.com>
2024-04-12 14:07:19 +0300

    Update FolderDto.cs

--- InfTech.Core/Dtos/FolderDto.cs
+++ InfTech.Core/Dtos/FolderDto.cs
@@ -1,8 +1,10 @@
-﻿namespace InfTech.Core.Dtos;
+namespace InfTech.Core.Dtos;
 
 public sealed class FolderDto
 {
     public int Id { get; init; }
 
     public string Name { get; init; } = string.Empty;
+
+    public int? ParentFolderId { get; init; }
 }
