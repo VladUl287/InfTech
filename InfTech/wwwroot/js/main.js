8ab1df36a3fe162dd03c7aa4f64653da35b49b0f
ferry <74259838+VladUl287@users.noreply.github.com>
2024-04-12 14:06:43 +0300

    Update main.js

--- InfTech/wwwroot/js/main.js
+++ InfTech/wwwroot/js/main.js
@@ -1,12 +1,3 @@
-function reloadTree() {
-    const tree = $('.tree').empty()
-    $.when($.get('/Folder/Get'), $.get('/File/Get'))
-        .then((folders, files) => {
-            tree.append(folders[0])
-            tree.append(files[0])
-        })
-}
-
 function rootEvent() {
     if (actionMode.mode === mode.createFolder) {
         $.get('/Folder/Create').then(setModalContent)
